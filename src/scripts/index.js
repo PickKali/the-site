0b6d521ff506e7f4dae7520c0155d727c1fe6fb2
PickKali <kali.pulanco@gmail.com>
2025-02-19 16:04:43 -0800

    Moved index script to separate js file; added embed images (finally)

---
+++ src/scripts/index.js
@@ -0,0 +1,98 @@
+const quotetable = [
+  { quote: "i'm now known as a defenestration girl", date: "1 March 2021" },
+  {
+    quote: "i'm having too much fun with these obbies",
+    date: "23 December 2023",
+  },
+  { quote: "i'm so glad that this is a feature", date: "28 June 2022" },
+  { quote: "proud to say that i'm a yuki main", date: "29 June 2021" },
+  {
+    quote: "y'all forgot that i'm actually a clown car?",
+    date: "20 June 2021",
+  },
+  {
+    quote: "dissociative amnesia gets to me but i'm still alive",
+    date: "4 October 2020",
+  },
+  {
+    quote:
+      "i'm 99% sure everyone here has played the lil nas x concert experience",
+    date: "26 April 2021",
+  },
+  {
+    quote:
+      "yay starting another game project that im unsure if i'm going to complete",
+    date: "9 February 2021",
+  },
+  {
+    quote: "i'm actually pretty happy about this floor",
+    date: "19 July 2021",
+  },
+  {
+    quote: "i'm still adamant about hatweight existing",
+    date: "25 April 2024",
+  },
+  {
+    quote: "fixated so hard that i'm now top 10k in minesweeper",
+    date: "24 March 2024",
+  },
+  {
+    quote: "idk what i'm doing and nobody else does either",
+    date: "21 February 2021",
+  },
+  {
+    quote: "im like 1/3rd of the way complete and i'm improvising as I go",
+    date: "17 February 2021",
+  },
+  {
+    quote:
+      "this is why i'm majoring in frontend development; i can't forgive myself for letting this happen",
+    date: "28 September 2023",
+  },
+  {
+    quote:
+      "if i'm in world 7 with 23 stars and 1 hour on the timer i am doing something really wrong",
+    date: "19 July 2024",
+  },
+  {
+    quote:
+      "i know that i'm a runner at my core; i don't know what i Speed Run 4",
+    date: "28 March 2024",
+  },
+  { quote: "i'm on my 28th watermelon", date: "27 April 2020" },
+  { quote: "i am sleeping on the ttyd ost", date: "2 September 2024" },
+  {
+    quote: "i wouldn't be surprised if you don't know who i am",
+    date: "1 September 2024",
+  },
+  { quote: "I am the definition of skill issue", date: "24 January 2024" },
+  {
+    quote: "unfortunately i am neither a guy or from an anime",
+    date: "29 March 2021",
+  },
+  { quote: "why am i having fun programming", date: "23 February 2024" },
+  { quote: "clearly, I am underleveled.", date: "26 November 2021" },
+  {
+    quote: "I wanna do devex but I am too lazy to do the tax forms",
+    date: "4 February 2023",
+  },
+  {
+    quote:
+      "im just an incredibly neurodivergent person that has nothing better to do",
+    date: "13 June 2024",
+  },
+  {
+    quote:
+      "idk if im a lexographer... lexicographer... see i can't even spell it",
+    date: "2 February 2024",
+  },
+  {
+    quote: "im like the older stepsibling that shows up every third episode",
+    date: "21 May 2024",
+  },
+];
+let assigned = quotetable[Math.floor(Math.random() * quotetable.length)];
+$("#randomquote").html(
+  '"' + assigned.quote + '"<br/><br/>Kali on ' + assigned.date,
+);
+//Newlines don't work in blockquotes!
