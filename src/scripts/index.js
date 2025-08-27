417e7bbddee2dfd7acee91e6336e6bf01e11f4d0
PickKali <kali.pulanco@gmail.com>
2025-08-26 21:34:06 -0700

    I did the thing again
    Look, I know it's a bad habit to put a lot of changes into a single commit despite not even planning to push said commit. Just let me be.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,5 @@
 const quotetable = [
   { quote: "i'm now known as a defenestration girl", date: "1 March 2021" },
-  {
-    quote: "i'm having too much fun with these obbies",
-    date: "23 December 2023",
-  },
   { quote: "i'm so glad that this is a feature", date: "28 June 2022" },
   { quote: "proud to say that i'm a yuki main", date: "29 June 2021" },
   {
@@ -13,11 +9,6 @@ const quotetable = [
   {
     quote: "dissociative amnesia gets to me but i'm still alive",
     date: "4 October 2020",
-  },
-  {
-    quote:
-      "i'm 99% sure everyone here has played the lil nas x concert experience",
-    date: "26 April 2021",
   },
   {
     quote:
@@ -51,16 +42,10 @@ const quotetable = [
   },
   {
     quote:
-      "if i'm in world 7 with 23 stars and 1 hour on the timer i am doing something really wrong",
-    date: "19 July 2024",
-  },
-  {
-    quote:
       "i know that i'm a runner at my core; i don't know what i Speed Run 4",
     date: "28 March 2024",
   },
   { quote: "i'm on my 28th watermelon", date: "27 April 2020" },
-  { quote: "i am sleeping on the ttyd ost", date: "2 September 2024" },
   {
     quote: "i wouldn't be surprised if you don't know who i am",
     date: "1 September 2024",
@@ -87,8 +72,33 @@ const quotetable = [
     date: "2 February 2024",
   },
   {
-    quote: "im like the older stepsibling that shows up every third episode",
-    date: "21 May 2024",
+    quote:
+      "i'm totally down to have a cooking animation where every grain of rice is individually animated",
+    date: "1 November 2024",
+  },
+  {
+    quote: "i'm parbroiled and sous vide 🔥 🔥 🚰",
+    date: "5 May 2025",
+  },
+  {
+    quote: "i have a funky progression",
+    date: "9 March 2023",
+  },
+  {
+    quote:
+      "i find it funny when developers don't know details of their own game",
+    date: "28 February 2023",
+  },
+  { quote: "i think its april fools 2023", date: "22 January 2023" },
+  { quote: "it seems i glitched through the wrong wall", date: "21 July 2021" },
+  {
+    quote: "scrapping it as i do with every insane idea i get",
+    date: "19 November 2023",
+  },
+  {
+    quote:
+      "doing my development style where i unhinge my mind and make whatever i feel like making",
+    date: "4 September 2024",
   },
 ];
 let assigned = quotetable[Math.floor(Math.random() * quotetable.length)];
@@ -96,3 +106,67 @@ $("#randomquote").html(
   '"' + assigned.quote + '"<br/><br/>Kali on ' + assigned.date,
 );
 //Newlines don't work in blockquotes!
+
+$(function () {
+  const posts = window.__POSTS__;
+
+  function renderPosts() {
+    const sort = $("#sortSelect").val();
+    const selectedTags = $(".tag-checkbox:checked")
+      .map(function () {
+        return $(this).val();
+      })
+      .get();
+
+    let filtered = posts;
+
+    if (selectedTags.length > 0) {
+      filtered = filtered.filter((p) =>
+        p.tags.some((tag) => selectedTags.includes(tag)),
+      );
+      $("#tagsummary").html(selectedTags.length + " tag(s) selected");
+    } else {
+      $("#tagsummary").html("Select Tags (Inclusive)");
+    }
+
+    filtered.sort((a, b) => {
+      if (sort === "Added Date (Descending)")
+        return new Date(b.addedDate) - new Date(a.addedDate);
+      if (sort === "Added Date (Ascending)")
+        return new Date(a.addedDate) - new Date(b.addedDate);
+      if (sort === "Based On Date (Descending)")
+        return new Date(b.relevantDate) - new Date(a.relevantDate);
+      if (sort === "Based On Date (Ascending)")
+        return new Date(a.relevantDate) - new Date(b.relevantDate);
+      return 0;
+    });
+    $("#sortsummary").html(sort);
+
+    $("#postlist").html(
+      filtered
+        .map((p) => {
+          const formatter = new Intl.DateTimeFormat("en-US", {
+            timeZone: "America/Los_Angeles",
+            dateStyle: "long",
+          });
+          const rdate = new Date(p.relevantDate);
+          const adate = new Date(p.addedDate);
+
+          return `<article>
+            <a href="${p.url}">${p.title}</a>
+            <br>
+            <small>
+              Based On: ${formatter.format(rdate)}<br>
+              Added: ${formatter.format(adate)}
+            </small>
+          </article>`;
+        })
+        .join(""),
+    );
+  }
+
+  renderPosts();
+
+  $("#sortselect").on("change", renderPosts);
+  $(".tag-checkbox").on("change", renderPosts);
+});
